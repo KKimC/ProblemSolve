fa03ca53a4cc07cb097f93ced2355224686fdb52
KKimC <99385873+KKimC@users.noreply.github.com>
2023-02-24 11:34:52 +0900

    [Gold V] Title: 부분 삼각 수열, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1548. 부분 삼각 수열/부분 삼각 수열.py
+++ 백준/Gold/1548. 부분 삼각 수열/부분 삼각 수열.py
@@ -3,8 +3,7 @@ nums=sorted(list(map(int,input().split())))
 answer=1
 for x in range(n-1):
     for z in range(n-1,-1,-1):
-            if z<x+1:
-                continue
+            if z<x+1:break
             if nums[x]+nums[x+1]>nums[z]:
                 answer=max(z-x+1,answer)
 print(answer)
