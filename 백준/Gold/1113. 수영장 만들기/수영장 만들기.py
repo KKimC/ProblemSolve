3599d64299f3134f684e6693a19a6ae004566024
KKimC <99385873+KKimC@users.noreply.github.com>
2023-03-24 17:40:47 +0900

    [Gold I] Title: 수영장 만들기, Time: 80 ms, Memory: 34192 KB -BaekjoonHub

--- 백준/Gold/1113. 수영장 만들기/수영장 만들기.py
+++ 백준/Gold/1113. 수영장 만들기/수영장 만들기.py
@@ -1,43 +1,33 @@
-
 from collections import deque
-n,m = map(int,input().split())
-graph = [list(map(int,list(input()))) for _ in range(n)]
-dx1 = [(-1,0),(0,1),(1,0),(0,-1)]
-new_graph = [i[::] for i in graph]
-def bfs(x,y):
-    global cnt
-    q = deque([(x,y)])
-    h = []
-    qq = deque([(x,y)])
+
+def bfs(x, y, height):
+    q = deque([(x, y)])
+    visited[x][y] = 1
+    cnt = 1
+    flag = False
     while q:
-        xx,yy = q.popleft()
-        for dx,dy in dx1:
-            nx = xx+dx
-            ny = yy+dy
-            if 0>nx or n<=nx or ny<0 or m<=ny:
-                return 0,0
-            if visited[nx][ny]:continue
-            if graph[nx][ny] > graph[x][y]:
-                h.append(graph[nx][ny])
+        x, y = q.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if (0 <= nx < n) and (0 <= ny < m):
+                if (not visited[nx][ny]) and (graph[nx][ny] <= height):
+                    q.append((nx, ny))
+                    visited[nx][ny] = 1
+                    cnt += 1
             else:
-                visited[nx][ny] = True
-                q.append((nx,ny))
-                qq.append((nx,ny))
-    minn = min(h)
-    while qq:
-        xx,yy = qq.popleft()
-        if minn > new_graph[xx][yy]:
-            cnt += minn - new_graph[xx][yy]
-            new_graph[xx][yy] = minn
-    return
-dic = {1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[],9:[]}
-visited = [[False]*m for _ in range(n)]
-cnt = 0
-for i in range(n):
-    for j in range(m):
-        for ii in range(n):
-            for jj in range(m):
-                visited[ii][jj] = False
-        visited[ii][jj] = True
-        bfs(i,j)
-print(cnt)
+                flag = True
+
+    return cnt if not flag else 0
+
+n,m = map(int, input().split())
+graph = [list(map(int, input().strip())) for _ in range(n)]
+dx, dy = [-1, 1, 0, 0], [0, 0, 1, -1]
+ans = 0
+for height in range(1, 9):
+    visited = [[0] * m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            if (not visited[i][j]) and (graph[i][j] <= height):
+                ans += bfs(i, j, height)
+
+print(ans)
